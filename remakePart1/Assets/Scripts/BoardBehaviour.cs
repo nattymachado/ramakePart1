bcf3a0571482b0d1f93e77a34b5bfec0daf42426
Natalia Machado <natalia.machado@hp.com>
2018-05-27 09:31:08 -0500

    Including moviment on the pill

--- remakePart1/Assets/Scripts/BoardBehaviour.cs
+++ remakePart1/Assets/Scripts/BoardBehaviour.cs
@@ -5,31 +5,65 @@ using UnityEngine;
 public class BoardBehaviour : MonoBehaviour {
 
     public GameObject prefab;
+    private Camera camera;
+    public GameObject pill = null;
+    private GameObject pillPart2 = null;
+    private SpriteRenderer board = null;
+
 
     // Use this for initialization
-    void Start () {
-        int i;
-        for (i=0; i < 20; i++)
-        {
-            Debug.Log(i);
-            float y = i * 1;
-            float x = i * Random.value;
-            Debug.Log(x);
-            Vector3 pos = new Vector3(x, y, 0);
-            var myNewObject = Instantiate(prefab, pos, Quaternion.Euler(0, 0, 180));
-            myNewObject.GetComponent<SpriteRenderer>().color = Color.red;
-            Vector3 pos2 = new Vector3(x+0.3f, y, 0);
-            var myNewObject2 = Instantiate(prefab, pos2, Quaternion.Euler(0, 0, 0));
-            myNewObject2.GetComponent<SpriteRenderer>().color = Color.yellow;
-        }
+    public void Start () {
 
+        board = GetComponent<SpriteRenderer>();
+        Debug.Log(board.transform.localPosition);
+        Debug.Log(board.size);
+        Debug.Log(board.size.y/2);
+        camera = Camera.main;
+    }
+    
+	// Update is called once per frame
+	public void Update () {
+        
+        if (pill == null)
+        {
+            createNewPill(1);
+
+        } else
+        {
+            if (board.transform.localPosition.y > (pill.transform.localPosition.y - 0.05f))
+            {
+                pill = null;
+            }
+            //Debug.Log(pill.transform.position);
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Debug.Log("Left");
+            }
+        }
+        
+        
 
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-       
+    private void createNewPill(int id)
+    {
+
+        
+        Vector3 pos = new Vector3(0, board.size.y, 0);
+        pill = Instantiate(prefab, pos, Quaternion.Euler(0, 0, 0));
+        SpriteRenderer[] pillParts =  pill.GetComponentsInChildren<SpriteRenderer>();
+        Debug.Log(pillParts[0].size);
+        pillParts[0].color = Color.yellow;
+        pillParts[1].color = Color.red;
+
+        float initial_y = board.size.y;
+        float initial_x = - (board.size.x - (pillParts[0].size.x * 2.4f));
+        Vector3 newPosition = new Vector3(initial_x, initial_y, 0);
+
+        pill.transform.parent = board.transform;
+        pill.transform.localPosition = newPosition;
+
+
 
     }
 }
---
+++ remakePart1/Assets/Scripts/PillBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillBehaviour : MonoBehaviour {
+
+    private float _totalTime;
+
+	// Use this for initialization
+	public void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    public void Update () {
+        _totalTime += Time.deltaTime;
+        BoardBehaviour parent = transform.parent.GetComponent<BoardBehaviour>();
+        Debug.Log(parent.pill);
+        if (parent.pill != null)
+        {
+            if (transform.parent.localPosition.y < (transform.localPosition.y - 0.05f))
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.05f, 0f);
+            }
+        }
+        
+        
+    }
+}
