1919bbca58758a8c52abc428b93713a2f6551af6
Luísa Cecília <34582911+LuCecyl@users.noreply.github.com>
2018-06-15 20:58:57 -0300

    Add files via upload

---
+++ screen/Assets/Scripts/player_selector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class player_selector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, -2.65f, transform.position.z); 
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, -1.975f, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (transform.position.y == -1.975f)
+            {
+                SceneManager.LoadScene("single_player_menu");
+            }
+            else
+            {
+                SceneManager.LoadScene("multiplayer_menu");
+            }
+        }
+    }
+
+
+}
